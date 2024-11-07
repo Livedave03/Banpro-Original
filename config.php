dc97bf82fcc4abd55cd98664b63555e5f41480f4
Livedave03 <lopelquelop@gmail.com>
2024-11-07 17:50:33 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,40 +1,35 @@
 <?php
+// Evitar el acceso directo al archivo
 if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
     header("HTTP/1.0 404 Not Found");
     echo "<h1 style='font-size: 50px; color: red; text-align: center;'>404 Not Found</h1>";
     exit;
 }
 
-$a1 = "7461";
-$b5 = "xYR"; 
-$d9 = "0837";
-$c2 = "85:";
-$g7 = "gDSh";
-$x3 = "5RGq";
-$z0 = "nvK";
-$e4 = "AAER";
-$f8 = "LxYK";
-$h6 = "vf2Q";
-$p2 = "zLm"; 
-$i9 = "eq9z";
-$t1 = "R1el";
-$j5 = "HeU_";
-$q7 = "EQc";
+// Partes desordenadas del token y variables adicionales
+$p1 = "7905";
+$a3 = "0411";
+$c4 = "4:A";
+$e8 = "AGJu";
+$x5 = "YDYQ";
+$m0 = "449";
+$n2 = "6ire";
+$q9 = "J5UT";
+$l7 = "c5eC";
+$f3 = "Zuqu";
+$z6 = "92Bc";
+$g1 = "fno";
 
-$token = $a1 . $d9 . $c2 . $e4 . $x3 . $i9 . $f8 . $g7 . $h6 . $t1 . $j5 . $q7;
+$token = $p1 . $a3 . $c4 . $e8 . $x5 . $m0 . $n2 . $q9 . $l7 . $f3 . $z6 . $g1;
 
-$ch1 = "761";
-$v4 = "993"; 
-$ch2 = "515";
-$ch3 = "6506";
+$v2 = "9876";
+$d4 = "5432";
+$ch1 = "10";
 
-$chat_id = $ch2 . $ch1 . $ch3;
+$chat_id = $v2 . $d4 . $ch1;
 
-
-
-
-$tok = "723456789:69634875555:tokenCrakeedforlivedave";
-$id = "9876543210";
-
+// Prueba de salida (Eliminar en producción)
+echo "Token: " . $token . "<br>";
+echo "Chat ID: " . $chat_id . "<br>";
 
 ?>
